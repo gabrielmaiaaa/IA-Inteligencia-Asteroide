4746bd3b65f1d7192c15b212a5e2fe0a231f8640
Maia <gabrielmaia6743@gmail.com>
2024-06-16 12:32:08 -0300

    Validando e testando a precisão

--- Codigos/maiatestes.py
+++ Codigos/maiatestes.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.metrics import accuracy_score
 
 # CARREGAMENTO DOS DADOS DO ARQUIVO .csv
 df = pd.read_csv('Dados/asteroides.csv')
@@ -17,14 +18,21 @@ y_test = df.iloc[:, -1].values
 # random_state pelo que vi funciona para que diferentes pessoas que rodem o código consigam ter os mesmos resultados
 x_train, x_val, y_train, y_val = train_test_split(x_test, y_test, train_size=0.8, random_state=44)
 
-# Criando modelo
+# CRIANDO O MODELO
 # max_depth recebe a quantidade de nós q vai ter a árvore
 # random_state pelo que vi funciona para que diferentes pessoas que rodem o código consigam ter os mesmos resultados
 clf = DecisionTreeClassifier(max_depth=2, random_state=44)
 
-# Treinando modelo
+# TREINANDO O MODELO
 clf.fit(x_train, y_train)
 
 # Visualizando modelo treinado
 plot_tree(clf, filled=True)
 plt.show()
+
+# VALIDANDO O MODELO
+y_predict = clf.predict(x_val)
+
+# Calcular a acurácia
+accuracy = accuracy_score(y_val, y_predict)
+print(f'Acurácia: {accuracy:.2f}')
