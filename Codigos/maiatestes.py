5e1735f81d09418edca7cd47f60a3a54f76cd3db
Maia <gabrielmaia6743@gmail.com>
2024-06-16 12:14:46 -0300

    Tratando dados

---
+++ Codigos/maiatestes.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+# CARREGAMENTO DOS DADOS DO ARQUIVO .csv
+df = pd.read_csv('Dados/asteroides.csv')
+
+# VARIÁVEIS USADAS COMO SEPARAÇÃO DOS VALORES DO ARQUIVO QUE QUEREMOS
+# Váriavel que contém dados do arquivo que vai até a penúltima coluna
+x_test = df.iloc[:, :-1].values
+# Váriavel que contém dados da última coluna do arquivo
+y_test = df.iloc[:, -1].values
+
+# Separando o treino e validação do nosso dado
+# train_size é usado para distribuir os dados entre a variável de treinamento e de validação (no nosso caso distribui em 80%)
+# random_state pelo que vi funciona para que diferentes pessoas que rodem o código consigam ter os mesmos resultados
+x_train, x_val, y_train, y_val = train_test_split(x_test, y_test, train_size=0.8, random_state=44)
+
