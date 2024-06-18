316a18619af2e62d5dd1dea8eeed3ee36becc4d9
teukuka <teukuka.silva@hotmail.com>
2024-06-18 09:01:35 -0300

    jupter quase pronto
    jupiter

--- Codigos/testes.py
+++ Codigos/testes.py
@@ -8,6 +8,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import MultinomialNB
+<<<<<<< Updated upstream
 
 def arvore_decicao(x_train, x_test, y_train, y_test):
     #cia a arvore de decicao
@@ -24,6 +25,60 @@ def arvore_decicao(x_train, x_test, y_train, y_test):
     #     max_features=None,      # Considera a raiz quadrada do número total de recursos para cada divisão
     #     random_state=None           # Controla a aleatoriedade para reproducibilidade
     # )
+=======
+from imblearn.over_sampling import SMOTE
+from sklearn.preprocessing import StandardScaler
+def arvore_decicao(x_train, x_test, y_train, y_test):
+    #cria a arvore de decicao
+        
+        #padrão
+    # clf = tree.DecisionTreeClassifier()
+    
+        # colocando os valores maior acurracy
+    # clf = tree.DecisionTreeClassifier(
+    #         ccp_alpha= 0.01,
+    #         criterion= 'entropy',
+    #         max_depth= None,
+    #         max_features= None,
+    #         max_leaf_nodes= None,
+    #         min_impurity_decrease= 0.0,
+    #         min_samples_leaf= 1,
+    #         min_samples_split= 2,
+    #         min_weight_fraction_leaf= 0.0,
+    #         splitter= 'best',
+    #         random_state=42,
+    #         class_weight='balanced'
+    # )
+         
+         
+    clf = tree.DecisionTreeClassifier(
+        criterion= 'entropy',
+        max_depth= 40, 
+        min_samples_leaf= 1,
+        min_samples_split= 2, 
+        splitter= 'best',
+        #random_state=42,
+        #class_weight='balanced'
+)
+         #colocando maior valor de perigo
+    # clf = tree.DecisionTreeClassifier(
+    #         criterion= 'entropy', 
+    #         ccp_alpha= 0.0, 
+    #         max_depth= None, 
+    #         max_features= None, 
+    #         max_leaf_nodes= None, 
+    #         min_impurity_decrease= 0.0, 
+    #         min_samples_leaf= 1, 
+    #         min_samples_split= 2, 
+    #         min_weight_fraction_leaf= 0.0, 
+    #         splitter= 'best',
+    #         random_state=42,
+    #         class_weight='balanced'
+    # )
+
+#     #treina a arvore
+#     clf.fit(x_train,y_train)
+>>>>>>> Stashed changes
     
         #mexendo valores
     # clf = tree.DecisionTreeClassifier(
@@ -41,6 +96,7 @@ def arvore_decicao(x_train, x_test, y_train, y_test):
     #treina a arvore
     clf.fit(x_train, y_train)
     
+<<<<<<< Updated upstream
     #faz o predict das amostras que a gente deixou como teste
     y_pred = clf.predict(x_test)
     
@@ -53,6 +109,55 @@ def arvore_decicao(x_train, x_test, y_train, y_test):
     disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
     disp.plot()
     plt.show()
+=======
+#     #faz a matriz de confusão
+#     cm = confusion_matrix(y_test, y_pred, labels=clf.classes_)
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
+#     disp.plot()
+#     plt.show()
+
+# def arvore_decicao(x_train, x_test, y_train, y_test, scoring='recall'):
+#     # Inicializar um DecisionTreeClassifier
+#     clf = tree.DecisionTreeClassifier(random_state=42, class_weight='balanced')
+
+#     # Definir o grid de parâmetros
+#     param_grid = {
+#         'criterion': ['gini', 'entropy', 'log_loss'],
+#         'splitter': ['best'],
+#         'max_depth': [None, 5, 10, 20],
+#         'min_samples_split': [2, 10, 20],
+#         'min_samples_leaf': [1, 5, 10],
+#         'min_weight_fraction_leaf': [0.0, 0.1],
+#         'max_features': [None, 'sqrt', 'log2'],
+#         'max_leaf_nodes': [None, 10],
+#         'min_impurity_decrease': [0.0, 0.01],
+#         'ccp_alpha': [0.0, 0.01, 0.1]
+#     }
+
+#     # Realizar busca em grade com validação cruzada
+#     grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, scoring=scoring, n_jobs=-1, verbose=2)
+#     grid_search.fit(x_train, y_train)
+
+#     # Melhor combinação de hiperparâmetros
+#     print("Melhores parâmetros encontrados: ", grid_search.best_params_)
+
+#     # Treinar o modelo com os melhores parâmetros
+#     best_clf = grid_search.best_estimator_
+#     best_clf.fit(x_train, y_train)
+
+#     # Fazer previsões no conjunto de teste
+#     y_pred = best_clf.predict(x_test)
+
+#     # Imprimir o relatório de classificação
+#     target_names = ['não perigoso', 'perigoso']
+#     print(classification_report(y_test, y_pred, target_names=target_names))
+
+#     # Matriz de confusão
+#     cm = confusion_matrix(y_test, y_pred)
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
+#     disp.plot()
+#     plt.show()
+>>>>>>> Stashed changes
 
 def MLP(x_train, x_test, y_train, y_test):
     # Crie o classificador MLP
@@ -162,11 +267,27 @@ def main():
     y = df['perigo']
 
     #separa em grupo de teste, para ver a eficacia do classificação
+<<<<<<< Updated upstream
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
     print("Tamanho do conjunto de treino:", x_train.shape)
     print("Tamanho do conjunto de teste:", x_test.shape)
     #MLP(x_train, x_test, y_train, y_test)
     #arvore_decicao(x_train, x_test, y_train, y_test)
+=======
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
+    # #     # Aplicar SMOTE para lidar com desbalanceamento
+    # smote = SMOTE(random_state=42)
+    # x_train, y_train = smote.fit_resample(x_train, y_train)
+
+    # Normalizar os dados
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+    #print("Tamanho do conjunto de treino:", x_train.shape)
+    #print("Tamanho do conjunto de teste:", x_test.shape)
+    MLP(x_train, x_test, y_train, y_test)
+    #arvore_decicao(x_train, x_test, y_train, y_test, scoring='recall')
+>>>>>>> Stashed changes
     #knn(x_train, x_test, y_train, y_test)
     #svc(x_train, x_test, y_train, y_test)
     naive(x_train, x_test, y_train, y_test)
