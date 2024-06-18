08267bc2ebe75cc27bd68ebc71cbd78923a289c6
teukuka <teukuka.silva@hotmail.com>
2024-06-17 22:28:53 -0300

    algo
    a

--- Codigos/testes.py
+++ Codigos/testes.py
@@ -1,9 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import tree
-from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -12,20 +11,41 @@ from sklearn.naive_bayes import MultinomialNB
 def arvore_decicao(x_train, x_test, y_train, y_test):
     #cia a arvore de decicao
         #padrão
-    clf = tree.DecisionTreeClassifier()
+    # clf = tree.DecisionTreeClassifier()
     
-        #colocando os valores padrão
-    # clf = tree.DecisionTreeClassifier(
-    #     criterion='gini',      # Usa ganho de informação como critério
-    #     splitter='best',
-    #     max_depth=None,             # Define a profundidade máxima da árvore
-    #     min_samples_split=2 ,      # Define o número mínimo de amostras para dividir um nó
-    #     min_samples_leaf=1,       # Define o número mínimo de amostras em um nó folha
-    #     max_features=None,      # Considera a raiz quadrada do número total de recursos para cada divisão
-    #     random_state=None           # Controla a aleatoriedade para reproducibilidade
-    # )
+        # colocando os valores maior acurracy
+    clf = tree.DecisionTreeClassifier(
+        'criterion': 'entropy', 
+        'ccp_alpha': 0.0, 
+        'max_depth': None, 
+        'max_features': None, 
+        'max_leaf_nodes': None, 
+        'min_impurity_decrease': 0.0, 
+        'min_samples_leaf': 1, 
+        'min_samples_split': 2, 
+        'min_weight_fraction_leaf': 0.0, 
+        'splitter': 'best'
+        )
+        
     
-        #mexendo valores
+    
+    clf = tree.DecisionTreeClassifier(
+        criterion='gini', 
+        splitter='best', 
+        max_depth=None, 
+        min_samples_split=2, 
+        min_samples_leaf=1, 
+        #min_weight_fraction_leaf=0.0, 
+        max_features='log2', 
+        #random_state=None, 
+        #max_leaf_nodes=None, 
+        #min_impurity_decrease=0.0, 
+        #class_weight=None, 
+        #ccp_alpha=0.0, 
+        #monotonic_cst=None
+    )
+    
+    #     mexendo valores
     # clf = tree.DecisionTreeClassifier(
     #     criterion='gini',      # Usa ganho de informação como critério
     #     splitter='best',
@@ -54,33 +74,79 @@ def arvore_decicao(x_train, x_test, y_train, y_test):
     disp.plot()
     plt.show()
 
+# def arvore_decicao(x_train, x_test, y_train, y_test, scoring='recall'):
+#     # Inicializar um DecisionTreeClassifier
+#     clf = tree.DecisionTreeClassifier(random_state=42, class_weight='balanced')
+
+#     # Definir o grid de parâmetros
+#     param_grid = {
+#         'criterion': ['gini', 'entropy', 'log_loss'],
+#         'splitter': ['best'],
+#         'max_depth': [None, 5, 10, 20],
+#         'min_samples_split': [2, 10, 20],
+#         'min_samples_leaf': [1, 5, 10],
+#         'min_weight_fraction_leaf': [0.0, 0.1],
+#         'max_features': [None, 'sqrt', 'log2'],
+#         'max_leaf_nodes': [None, 10],
+#         'min_impurity_decrease': [0.0, 0.01],
+#         'ccp_alpha': [0.0, 0.01, 0.1]
+#     }
+
+#     # Realizar busca em grade com validação cruzada
+#     grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, scoring=scoring, n_jobs=-1, verbose=2)
+#     grid_search.fit(x_train, y_train)
+
+#     # Melhor combinação de hiperparâmetros
+#     print("Melhores parâmetros encontrados: ", grid_search.best_params_)
+
+#     # Treinar o modelo com os melhores parâmetros
+#     best_clf = grid_search.best_estimator_
+#     best_clf.fit(x_train, y_train)
+
+#     # Fazer previsões no conjunto de teste
+#     y_pred = best_clf.predict(x_test)
+
+#     # Imprimir o relatório de classificação
+#     target_names = ['não perigoso', 'perigoso']
+#     print(classification_report(y_test, y_pred, target_names=target_names))
+
+#     # Matriz de confusão
+#     cm = confusion_matrix(y_test, y_pred)
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
+#     disp.plot()
+#     plt.show()
+
+
 def MLP(x_train, x_test, y_train, y_test):
     # Crie o classificador MLP
     
-    mlp = MLPClassifier()
-    # hidden_layer_sizes=(512, 256, 128),  # Aumenta o número de neurônios e camadas ocultas
+    # mlp = MLPClassifier(
+    # hidden_layer_sizes=(256, 128),  # Aumenta o número de neurônios e camadas ocultas
     # activation='relu',                   # Função de ativação
     # solver='adam',                       # Algoritmo otimizador
     # alpha=0.0001,                        # Regularização L2
     # batch_size=256,                      # Aumenta o tamanho dos lotes
     # learning_rate='adaptive',            # Taxa de aprendizado adaptativa
-    # learning_rate_init=0.001,            # Taxa de aprendizado inicial
-    # max_iter=1000,                       # Aumenta o número máximo de iterações
+    # learning_rate_init=0.00001,            # Taxa de aprendizado inicial
+    # max_iter=500,                       # Aumenta o número máximo de iterações
     # shuffle=True,                        # Embaralha os dados a cada iteração
     # random_state=42,                     # Semente para reprodução dos resultados
     # tol=1e-6,                            # Reduz a tolerância para otimização
     # verbose=False,                       # Imprime progresso durante a otimização
     # warm_start=False,                    # Reutiliza solução anterior
     # momentum=0.9,                        # Taxa de momento
-    # nesterovs_momentum=False,             # Usa momento de Nesterov
-    # early_stopping=True,                 # Parada antecipada
+    # nesterovs_momentum=True,             # Usa momento de Nesterov
+    # early_stopping=False,                 # Parada antecipada
     # validation_fraction=0.2,             # Fração de dados para validação
     # beta_1=0.9,                          # Taxa de decaimento exponencial para o primeiro momento
     # beta_2=0.999,                        # Taxa de decaimento exponencial para o segundo momento
     # epsilon=1e-08,                       # Valor para evitar divisão por zero
     # n_iter_no_change=20,                 # Número de iterações sem melhora
     # max_fun=30000                        # Número máximo de chamadas de função
-
+    # )
+    
+    mlp = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    hidden_layer_sizes=(5, 2), random_state=1)
 
     # Treine o classificador
     mlp.fit(x_train, y_train)
@@ -137,21 +203,59 @@ def svc(x_train, x_test, y_train, y_test):
     disp.plot()
     plt.show()
 
-def naive(x_train, x_test, y_train, y_test):
-    clf = MultinomialNB()
-    clf.fit(x_train, y_train)
-    #Faça previsões no conjunto de teste
-    y_pred = clf.predict(x_test)
+# def naive(x_train, x_test, y_train, y_test):
 
-    #faz o relatório de métricas
+#     clf = MultinomialNB()
+#     clf.fit(x_train, y_train)
+#     #Faça previsões no conjunto de teste
+#     y_pred = clf.predict(x_test)
+
+#     #faz o relatório de métricas
+#     target_names = ['não perigoso', 'perigoso']
+#     print(classification_report(y_test, y_pred, target_names=target_names))
+    
+#     #faz a matriz de confusão
+#     cm = confusion_matrix(y_test, y_pred, labels=clf.classes_)
+#     disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
+#     disp.plot()
+#     plt.show()
+
+def naive(x_train, x_test, y_train, y_test, scoring='recall'):
+    # Inicializar um MultinomialNB
+    clf = MultinomialNB()
+
+    # Definir o grid de parâmetros
+    param_grid = {
+        'alpha': [0.0, 0.1, 0.5, 1.0, 2.0, 5.0, 10.0],
+        'fit_prior': [True, False]
+    }
+
+    # Realizar busca em grade com validação cruzada
+    grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=5, scoring=scoring, n_jobs=-1, verbose=2)
+    grid_search.fit(x_train, y_train)
+
+    # Melhor combinação de hiperparâmetros
+    print("Melhores parâmetros encontrados: ", grid_search.best_params_)
+
+    # Treinar o modelo com os melhores parâmetros
+    best_clf = grid_search.best_estimator_
+    best_clf.fit(x_train, y_train)
+
+    # Fazer previsões no conjunto de teste
+    y_pred = best_clf.predict(x_test)
+
+    # Imprimir o relatório de classificação
     target_names = ['não perigoso', 'perigoso']
     print(classification_report(y_test, y_pred, target_names=target_names))
-    
-    #faz a matriz de confusão
-    cm = confusion_matrix(y_test, y_pred, labels=clf.classes_)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=['não perigoso', 'perigoso'])
+
+    # Matriz de confusão
+    cm = confusion_matrix(y_test, y_pred, labels=best_clf.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=target_names)
     disp.plot()
     plt.show()
+
+# Exemplo de uso (você precisa fornecer os dados de entrada apropriados)
+# naive(x_train, x_test, y_train, y_test)
 
 def main():
     #lê os arquivos
@@ -162,14 +266,14 @@ def main():
     y = df['perigo']
 
     #separa em grupo de teste, para ver a eficacia do classificação
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=42)
     print("Tamanho do conjunto de treino:", x_train.shape)
     print("Tamanho do conjunto de teste:", x_test.shape)
     #MLP(x_train, x_test, y_train, y_test)
-    #arvore_decicao(x_train, x_test, y_train, y_test)
+    arvore_decicao(x_train, x_test, y_train, y_test, scoring='recall')
     #knn(x_train, x_test, y_train, y_test)
     #svc(x_train, x_test, y_train, y_test)
-    naive(x_train, x_test, y_train, y_test)
+    #naive(x_train, x_test, y_train, y_test)
     
 if __name__ == "__main__":
     main()
